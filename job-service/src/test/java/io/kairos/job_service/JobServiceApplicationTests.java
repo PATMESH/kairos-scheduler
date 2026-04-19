20df51f969802257a1892492210f60b584aad521
PATMESH <patmeshpatmesh2003@gmail.com>
2026-04-19 23:58:05 +0530

    chore: base project structure — job-service, scheduler-service, execution-service, frontend

---
+++ job-service/src/test/java/io/kairos/job_service/JobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kairos.job_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
