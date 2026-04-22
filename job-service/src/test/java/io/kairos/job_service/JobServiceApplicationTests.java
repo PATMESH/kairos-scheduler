4f6927a8cdac2c1719309959bfe520eab481840a
PATMESH <patmeshpatmesh2003@gmail.com>
2026-04-22 22:07:35 +0530

    updated dependencies

--- job-service/src/test/java/io/kairos/job_service/JobServiceApplicationTests.java
+++ job-service/src/test/java/io/kairos/job_service/JobServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package io.kairos.job_service;
 
+import io.kairos.jobservice.JobServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = JobServiceApplication.class)
 class JobServiceApplicationTests {
 
 	@Test
